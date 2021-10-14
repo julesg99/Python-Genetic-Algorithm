bf228d3c449ba98bf4dbe8da85da4e59b4c67b8c
BLGFQY <54372768+BLGFQY@users.noreply.github.com>
2021-10-14 14:31:36 -0500

    created solution class and finished implementation of initial population

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import numpy
 
-popSize = 1000
+
+class Solution:
+
+    # constructor for creating a random solution
+    def __init__(self, gear):
+
+        self.knapsack = list()
+        self.totalUtil = 0
+        self.totalWeig = 0
+
+        while self.totalWeig < 500 or len(self.knapsack) < 20:
+            temp = gear[numpy.random.randint(0, 400)]
+            self.knapsack.append(temp)
+            self.totalWeig += temp[1]
+            self.totalUtil += temp[0]
+        print("solution = ", self.knapsack, "\nlength = ", len(self.knapsack), "\ntotal weight = ", self.totalWeig, "\ntotal utility = ", self.totalUtil, "\n")
+
+    # constructor for creating a solution based on two parents
+    # def __int__(self, parents):
+    #     population = []
+    #
+    # def mutation(self):
+    #     # stuff
+    #
+    # def selection(self):
+    #     # stuff
+    #
+    #     # necessary?
+    # def crossover(self):
+    #     # stuff
+
+# number of combinations
+popSize = 5
+# likelihood of a mutation happening
 muteRate = 0.0001
 
 # list of all possible items (utility, weight)
@@ -16,59 +49,10 @@ with open('Program2Input.txt', 'r') as file:
         count += 1
 
 
-# generation is a list of 1000 different solutions
-generation = []
-for i in range(0, popSize):
-    weight = 0
-    list = []
-    while weight < 500:
-        temp = numpy.random.randint(0, 400)
-        list.append(gear[temp])
-        weight += gear[temp][1]
-    generation.append(list)
-
-print(generation)
-
-scores = []
-for i in range(0, popSize):
-    totalUtil = 0
-    for j in range(0, len(generation[i])):
-        totalUtil += generation[i][j]
-
-print(scores)
-
-
-
-
-
-                                # popSize = (1000, 1000)
-# generation = numpy.random.randint(0, 401, popSize)
-# print(generation)
-
-# for i in range(0, len(generation)):
-#     weight = 0
-#     for j in range(0, len(generation[i])):
-#         weight += gear[generation[i][j]][1]
-#     print(weight/20)
-
-
-
-# a generation is presented as a list of integers, each value ranging from 0-400
-# each integer represents an index location in the gear list
-# building first generation
-# gen1 = []
-# weight = 0
-# for i in range(0, popSize[0]):
-#     gen1.append(random.randint(0, len(gear)-1))
-#     print(gear[gen1[i]][1])
-#     weight += gear[gen1[i]][1]
-# weight /= 20
-# print(weight)
-
-
-class solution:
-    gear =[]
-
+# attempting to create a generation of 1000 combinations
+generation = list()
+for i in range(popSize):
+    generation.append(Solution(gear))
 
 
 

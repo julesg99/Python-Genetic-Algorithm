c35c712d4aa88562c495f644bc5bd8c09c1fa908
BLGFQY <54372768+BLGFQY@users.noreply.github.com>
2021-10-14 21:25:24 -0500

    updated class constructor

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
 import numpy
 import random
 
+def selectSort(generation: list):
+    for i in range(popSize):
+        index = i
+        for j in range(i + 1, popSize):
+            if generation[index].totalUtil > generation[j].totalUtil:
+                index = j
+
+        generation[i], generation[index] = generation[index], generation[i]
+
+
+def selection(generation: list):
+    selectSort(generation)
+    index = len(generation)//2
+    if index % 2 == 1:
+        index += 1
+    generation = generation[index:]
+    return generation
+
+
 class Solution:
 
+    # attempting to implement constructors for specific arguments
     # constructor for creating a random solution
-    def __init__(self, gear):
+    def __init__(self, *num):
+        # default construction, when a random solution is needed
+        # argument is list of gear
+        if len(num) == 2:
+            self.knapsack = list()
+            self.totalUtil = 0
+            self.totalWeig = 0
 
-        self.knapsack = list()
-        self.totalUtil = 0
-        self.totalWeig = 0
+            while self.totalWeig < 500 or len(self.knapsack) < 20:
+                temp = gear[numpy.random.randint(0, 400)]
+                self.knapsack.append(temp)
+                self.totalWeig += temp[1]
+                self.totalUtil += temp[0]
 
-        while self.totalWeig < 500 or len(self.knapsack) < 20:
-            temp = gear[numpy.random.randint(0, 400)]
-            self.knapsack.append(temp)
-            self.totalWeig += temp[1]
-            self.totalUtil += temp[0]
- #       print("solution = ", self.knapsack, "\nlength = ", len(self.knapsack), "\ntotal weight = ", self.totalWeig, "\ntotal utility = ", self.totalUtil, "\n")
+            # rounding for readability
+            self.totalUtil, self.totalWeig = round(self.totalUtil, 2), round(self.totalWeig, 2)
+            # print("solution = ", self.knapsack, "\nlength = ", len(self.knapsack), "\ntotal weight = ", self.totalWeig, "\ntotal utility = ", self.totalUtil, "\n")
 
-    # constructor for creating a solution based on two parents
-    # def __int__(self, parents):
-    #     population = []
-    #
+        # constructor for creating a solution based on two parents, this is where CROSSOVER takes place
+        # argument is two solutions, one mama and one papa
+        elif len(num) == 3:
+            split = random.randint(0, len(num[0]))
+            self.knapsack = num[0][:split] + num[1][split:]
 
     def mutation(self, muteRate):
         # if a randomly generated value is less than the given mutation rate (0.0001) then a mutation *should* occur
         # each element in the list has an independent chance of mutation
         for i in range(len(self.knapsack)):
             if numpy.random.random() < muteRate:
-                print("true")
                 self.knapsack[i][1] = round(random.uniform(0.0, 25.0), 1)
 
 
 # number of combinations (spec = 1000)
 popSize = 10
-# likelihood of a mutation happening (spec = 0.0001
-muteRate = 0.1
+# likelihood of a mutation happening (spec = 0.0001)
+muteRate = 0.0001
 
 # list of all possible items (utility, weight)
 gear = []
@@ -44,8 +69,7 @@ with open('Program2Input.txt', 'r') as file:
     count = 0
     for i in file:
         gear.append(i.split())
-        gear[count][0] = float(gear[count][0])
-        gear[count][1] = float(gear[count][1])
+        gear[count][0], gear[count][1] = float(gear[count][0]), float(gear[count][1])
         count += 1
 
 
@@ -57,15 +81,26 @@ for i in range(popSize):
 # generation.sort(key=generation[1].getUtility())
 # sort(generation.totalUtil)
 
+generation = selection(generation)
+needed = popSize - len(generation)
+for i in range(needed):
+    mama = random.randint(0, len(generation))
+    papa = random.randint(0, len(generation))
+    generation.append(Solution(generation[mama], generation[papa]))
+
+
+
+
+## Testing
 for i in range(popSize):
-    index = i
-    for j in range(i+1, popSize):
-        if generation[index].totalUtil > generation[j].totalUtil:
-            index = j
+    print(generation[i].totalUtil, end=", ")
+print("length = ", len(generation))
 
-    generation[i], generation[index] = generation[index], generation[i]
+#selectSort(generation)
+generation = selection(generation)
 
-print(generation[0].knapsack)
-generation[0].mutation(muteRate)
-print(generation[0].knapsack)
+for i in range(len(generation)):
+    print(generation[i].totalUtil, end=", ")
+print("length = ", len(generation))
+
 

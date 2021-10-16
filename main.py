265585575eea8f19bc8c2c9b8ae6cabe245d84aa
BLGFQY <54372768+BLGFQY@users.noreply.github.com>
2021-10-16 18:02:33 -0500

    most of the necessary functions are at least partially implemented, selection function requires revision

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import numpy
 import random
 
+
+# function for sorting a generation based on utility score
 def selectSort(generation: list):
     for i in range(popSize):
         index = i
         for j in range(i + 1, popSize):
-            if generation[index].totalUtil > generation[j].totalUtil:
+            if generation[index].utility > generation[j].utility:
                 index = j
 
         generation[i], generation[index] = generation[index], generation[i]
 
-
+    # function decides which solutions of the previous generation will create the next generation
+    # currently it takes the better half of generation, based on utility
 def selection(generation: list):
     selectSort(generation)
     index = len(generation)//2
@@ -19,34 +22,60 @@ def selection(generation: list):
     generation = generation[index:]
     return generation
 
+    # prints out a list of all utilities in a generation
+def printGenUtil(generation: list):
+    for i in range(len(generation)):
+        print(generation[i].utility, end=", ")
+    print()
+
+# function performs L2 normalization (squared) on utility values of a generation
+def normalize(generation: list):
+    squaredSum = 0 # the sum of squared utility
+    for i in range(len(generation)):
+        squaredSum += numpy.square(generation[i].utility)
+
+    for i in range(len(generation)):
+        generation[i].normal = numpy.square(generation[i].utility) / squaredSum
+
+
+def getAverage(generation: list):
+    average = 0
+    for i in range(len(generation)):
+        average += generation[i].utility
+    average /= len(generation)
+    return average
 
 class Solution:
 
     # attempting to implement constructors for specific arguments
     # constructor for creating a random solution
     def __init__(self, *inp):
+        # declaring instance variables
         self.knapsack = list()
-        self.totalUtil = 0
-        self.totalWeig = 0
+        self.utility = 0
+        self.weight = 0
+        self.normal = 0
 
+        # execute constructor function if one argument is given
         if len(inp) == 1:
             self.constructor(inp)
 
+        # execute crossover function if two arguments are given
         elif len(inp) == 2:
             self.crossover(inp)
 
     # default construction, when a random solution is needed
     # argument is list of gear
     def constructor(self, args):
-        while self.totalWeig < 200 or len(self.knapsack) < 20:
+        while self.weight < 200 or len(self.knapsack) < 20:
             temp = gear[numpy.random.randint(0, 400)]
             self.knapsack.append(temp)
-            self.totalWeig += temp[1]
-            self.totalUtil += temp[0]
+            self.weight += temp[1]
+            self.utility += temp[0]
 
         # rounding for readability
-        self.totalUtil, self.totalWeig = round(self.totalUtil, 2), round(self.totalWeig, 2)
-        print("solution = ", self.knapsack, "\nlength = ", len(self.knapsack), "\ntotal weight = ", self.totalWeig, "\ntotal utility = ", self.totalUtil, "\n")
+        self.utility, self.weight = round(self.utility, 2), round(self.weight, 2)
+        # print("solution = ", self.knapsack, "\nlength = ", len(self.knapsack), "\ntotal weight = ", self.totalWeig, "\ntotal utility = ", self.totalUtil, "\n")
 
     # method creates a new solution by randomly splicing two parents together
     # constructor for creating a new solution by randomly splicing two parents, this is where CROSSOVER takes place
@@ -70,18 +99,18 @@ class Solution:
 
     def getTotalWeight(self):
         for i in range(len(self.knapsack)):
-            self.totalWeig += self.knapsack[1]
-        return self.totalWeig
+            self.weight += self.knapsack[1]
+        return self.weight
 
     def getTotalUtility(self):
         for i in range(len(self.knapsack)):
-            self.totalUtil += self.knapsack[0]
-        return self.totalUtil
+            self.utility += self.knapsack[0]
+        return self.utility
 
 
 ##### MAIN #####
 # number of combinations (spec = 1000)
-popSize = 10
+popSize = 1000
 # likelihood of a mutation happening (spec = 0.0001)
 muteRate = 0.0001
 
@@ -103,37 +132,27 @@ for i in range(popSize):
     generation.append(Solution(gear))
 
 
-generation.append(Solution(generation[0].knapsack, generation[1].knapsack).knapsack)
+
+##### Testing #####
+
+printGenUtil(generation)
+generation = selection(generation)
+printGenUtil(generation)
+
+#print(getAverage(generation))
 
 
 
-
-
-
-
-
-
-
+#
 # generation = selection(generation)
 # needed = popSize - len(generation)
 # for i in range(needed):
 #     mama = random.randint(0, len(generation))
 #     papa = random.randint(0, len(generation))
 #     generation.append(Solution(generation[mama], generation[papa]))
+#
 
 
 
-#
-# ##### Testing #####
-# for i in range(popSize):
-#     print(generation[i].totalUtil, end=", ")
-# print("length = ", len(generation))
-#
-# #selectSort(generation)
-# generation = selection(generation)
-#
-# for i in range(len(generation)):
-#     print(generation[i].totalUtil, end=", ")
-# print("length = ", len(generation))
 
 
